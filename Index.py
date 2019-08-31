bb7f02f3063093f6d0e4e65c4710c3d205792a88
Technical Manzoo <34577198+manzoo16@users.noreply.github.com>
2019-09-01 00:41:19 +0300

    Add files via upload

---
+++ Index.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+number_one = input('Enter the first number: ')
+number_two = input('Enter the second number: ')
+number_one = int(number_one)
+number_two = int(number_two)
+print('##    ##')
+print('# #  # #')
+print('#  #   #')
+print('#      #')
+print('The Manzoo program calculating Result....')
+print ('Please Wait..........')
+print('###########################################')
+result = number_one * number_two
+print ('The Result is: ' + str(result))
+    
+number_three = input('Enter the third number: ')
+number_four = input('Enter the fourth number: ')
+number_three = int(number_three)
+number_four = int(number_four)
+print('The program calculationg, please wait...')
+result = number_three * number_four
+print('The Result is: ' + str(result))
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
