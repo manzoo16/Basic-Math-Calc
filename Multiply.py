f1760275725470de7e5607c738352e9ab63797ba
Technical Manzoo <34577198+manzoo16@users.noreply.github.com>
2019-09-01 08:58:39 +0300

    Update Multiply.py

--- Multiply.py
+++ Multiply.py
@@ -4,29 +4,32 @@
 # In[ ]:
 
 
-number_one = input('Enter the first number: ')
-number_two = input('Enter the second number: ')
-number_one = int(number_one)
-number_two = int(number_two)
+print('\033[1;32mWelcome to the Python Math Operator Program - Multiply\033[1;32m')
+
 print ('\033[1;31m---------------------------------------------\033[1;m\n')
 print('''\033[1;34m##     ##     ##   ##    ##  #######  #####\033[1;m''')
 print('''\033[1;34m#### ####   ## ##  ####  ##    ###   #     #\033[1;m''')
 print('''\033[1;34m## ##  ##  ####### ##  ####  ###     #     #\033[1;m''')
 print('''\033[1;34m##     ## ##    ## ##    ## #######   #####\033[1;m''')
 print ('\033[1;31m---------------------------------------------\033[1;m\n')
+
+number_one = input('\033[36mEnter the first number: \033[36m')
+number_two = input('\033[36mEnter the second number: \033[36m')
+number_one = int(number_one)
+number_two = int(number_two)
 print('The Manzoo program calculating Result.... ')
 print('''\033[1;34mPlease Wait..........\033[1;m''')
 print('###########################################')
 result = number_one * number_two
 print ('The Result is: ' + str(result))
     
-number_three = input('Enter the third number: ')
-number_four = input('Enter the fourth number: ')
+number_three = input('\033[36mEnter the third number: \033[36m')
+number_four = input('\033[36mEnter the fourth number: \033[36m')
 number_three = int(number_three)
 number_four = int(number_four)
-print('The program calculating, please wait...')
+print('\033[1;32mThe program calculating, please wait...\033[1;32m')
 result = number_three * number_four
 print('The Result is: ' + str(result))
 
 print('Thank You For Using This Program')
-print('See You Again')
+print('....GoodBye')
