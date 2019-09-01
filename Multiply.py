5fd6e56881ad5a9f1df3602a2b987a7319a6ce10
Technical Manzoo <34577198+manzoo16@users.noreply.github.com>
2019-09-01 09:07:02 +0300

    Update Multiply.py

--- Multiply.py
+++ Multiply.py
@@ -6,6 +6,7 @@
 
 print('\033[1;32mWelcome to the Python Math Operator Program - Multiply\033[1;32m')
 
+
 print ('\033[1;31m---------------------------------------------\033[1;m\n')
 print('''\033[1;34m##     ##     ##   ##    ##  #######  #####\033[1;m''')
 print('''\033[1;34m#### ####   ## ##  ####  ##    ###   #     #\033[1;m''')
@@ -13,8 +14,10 @@ print('''\033[1;34m## ##  ##  ####### ##  ####  ###     #     #\033[1;m''')
 print('''\033[1;34m##     ## ##    ## ##    ## #######   #####\033[1;m''')
 print ('\033[1;31m---------------------------------------------\033[1;m\n')
 
-number_one = input('\033[36mEnter the first number: \033[36m')
-number_two = input('\033[36mEnter the second number: \033[36m')
+
+
+number_one = input('\033[36m[*]Enter the first number: \033[36m')
+number_two = input('\033[36m[*]Enter the second number: \033[36m')
 number_one = int(number_one)
 number_two = int(number_two)
 print('The Manzoo program calculating Result.... ')
@@ -22,14 +25,19 @@ print('''\033[1;34mPlease Wait..........\033[1;m''')
 print('###########################################')
 result = number_one * number_two
 print ('The Result is: ' + str(result))
+
+
     
-number_three = input('\033[36mEnter the third number: \033[36m')
-number_four = input('\033[36mEnter the fourth number: \033[36m')
+number_three = input('\033[36m[*]Enter the third number: \033[36m')
+number_four = input('\033[36m[*]Enter the fourth number: \033[36m')
 number_three = int(number_three)
 number_four = int(number_four)
 print('\033[1;32mThe program calculating, please wait...\033[1;32m')
 result = number_three * number_four
-print('The Result is: ' + str(result))
+print('\033[0mThe Result is: \033[0m' + str(result))
 
+
+print ('\033[1;31m---------------------------------------------\033[1;m\n')
 print('Thank You For Using This Program')
 print('....GoodBye')
+print ('\033[1;31m---------------------------------------------\033[1;m\n')
